10952f98789c3f96b8c1a40c05a28287fd79d17d
denisgenest <denisgenest@gmail.com>
2021-06-19 14:06:50 -0700

    this is the initial commit, creating the folder baby

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am an unfinished object, help!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
